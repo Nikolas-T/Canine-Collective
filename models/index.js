2aead845412049631126854d37a357188d6d78c7
AjaniBrowne <ajani.browne@yahoo.com>
2022-08-03 14:51:24 -0400

    commiton main " updated models index.js"

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Dogs = require('./dogs');
 
-User.hasMany(Project, {
+User.hasMany(Dogs, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Dogs.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Dogs };
