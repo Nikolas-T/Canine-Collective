e77db7f71caa8fd29f64e1f7636f345733a7530f
LorenzoF <lorenzoferritsic@gmail.com>
2022-08-08 14:25:52 -0400

    Fixed login routes in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,6 +15,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+  res.render('login');
+});
+
 router.get('/search/', async (req, res) => { 
   try {
     let options = {
